d2a9bbf22f2a793c2b4bee54fd524fcc7349c20d
wangxiaoyou1993 <xiaoyou@mage.ai>
2022-06-10 15:07:28 -0700

    [xy] temp work for dask.

--- mage_ai/data_cleaner/column_types/column_type_detector.py
+++ mage_ai/data_cleaner/column_types/column_type_detector.py
@@ -1,18 +1,25 @@
 from mage_ai.data_cleaner.column_types.constants import NUMBER_TYPES, ColumnType
+from mage_ai.data_cleaner.shared.logger import timer
 from mage_ai.data_cleaner.shared.multi import run_parallel_multiple_args
 from mage_ai.data_cleaner.transformer_actions.constants import (
     CONSTANT_IMPUTATION_DEFAULTS,
     INVALID_VALUE_PLACEHOLDERS,
 )
+import dask.dataframe as dd
+import multiprocessing
 import numpy as np
 import pandas as pd
 import re
 
+VERBOSE = True
+DASK_PARTITION_COUNT = 8
+
 DATETIME_MATCHES_THRESHOLD = 0.5
 MAXIMUM_WORD_LENGTH_FOR_CATEGORY_FEATURES = 40
-MULTITHREAD_MAX_NUM_ENTRIES = 50000
+MULTITHREAD_MAX_NUM_ENTRIES = 500000
 NUMBER_TYPE_MATCHES_THRESHOLD = 0.8
 STRING_TYPE_MATCHES_THRESHOLD = 0.3
+
 
 REGEX_DATETIME_PATTERN = r'^\d{2,4}-\d{1,2}-\d{1,2}$|^\d{2,4}-\d{1,2}-\d{1,2}[Tt ]{1}\d{1,2}:\d{1,2}[:]{0,1}\d{1,2}[\.]{0,1}\d*|^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$|^\d{1,4}[-\/]{1}\d{1,2}[-\/]{1}\d{1,2}$|^\d{1,2}[-\/]{1}\d{1,2}[-\/]{1}\d{1,4}$|^\d{1,2}[-\/]\d{1,2}[-\/]\d{2,4}[Tt ]{1}\d{1,2}:\d{1,2}[:]{0,1}\d{1,2}[\.]{0,1}\d*|(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+(\d{1,2})[\s,]+(\d{2,4})'
 REGEX_DATETIME = re.compile(REGEX_DATETIME_PATTERN)
@@ -132,14 +139,29 @@ def infer_column_type(series, column_name, dtype, kwargs):
 
 
 def infer_object_type(series, column_name, kwargs):
-    clean_series = series.apply(lambda x: x.strip(' \'\"') if type(x) is str else x)
-    clean_series = clean_series.map(lambda x: x if (not isinstance(x, str) or x != '') else np.nan)
-    clean_series = clean_series.dropna().astype(str)
+    with timer('column_type_detector.infer_object_type', dict(column_name=column_name)):
+        series_dask = dd.from_pandas(series, npartitions=DASK_PARTITION_COUNT)
+        return infer_object_type_internal(series_dask, column_name, kwargs)
 
-    series_nunique = series.nunique(dropna=False)
-    clean_series_nunique = clean_series.nunique()
 
-    exact_dtype = type(clean_series.iloc[0]) if clean_series.count() else None
+def infer_object_type_internal(series, column_name, kwargs):
+    with timer('column_type_detector.infer_object_type.part1_1', dict(column_name=column_name), verbose=VERBOSE):
+        clean_series = series.apply(lambda x: x.strip(' \'\"') if type(x) is str else x, meta=('column_name', 'object'))
+    with timer('column_type_detector.infer_object_type.part1_2', dict(column_name=column_name), verbose=VERBOSE):
+
+        clean_series = clean_series.map(lambda x: x if (not isinstance(x, str) or x != '') else np.nan)
+    with timer('column_type_detector.infer_object_type.part1_3', dict(column_name=column_name), verbose=VERBOSE):
+
+        clean_series = clean_series.dropna().astype(str)
+        clean_series_raw = clean_series.compute()
+        clean_series = dd.from_pandas(clean_series_raw, npartitions=DASK_PARTITION_COUNT)
+    with timer('column_type_detector.infer_object_type.part1_4', dict(column_name=column_name), verbose=VERBOSE):
+
+        series_nunique = series.nunique(dropna=False).compute()
+    with timer('column_type_detector.infer_object_type.part1_5', dict(column_name=column_name), verbose=VERBOSE):
+        clean_series_nunique = clean_series.nunique().compute()
+    with timer('column_type_detector.infer_object_type.part1_5', dict(column_name=column_name), verbose=VERBOSE):
+        exact_dtype = type(clean_series_raw.iloc[0]) if clean_series.count().compute() else None
     if exact_dtype is list:
         mdtype = ColumnType.TEXT
     elif np.issubdtype(exact_dtype, np.bool_):
@@ -151,13 +173,15 @@ def infer_object_type(series, column_name, kwargs):
         mdtype = ColumnType.TRUE_OR_FALSE
     else:
         length = len(clean_series)
-        if all(clean_series.str.match(REGEX_NUMBER)):
-            if clean_series.str.contains(REGEX_FLOAT_NEW_SYM).sum():
+        with timer('column_type_detector.infer_object_type.part2', dict(column_name=column_name), verbose=VERBOSE):
+            match_number_result = all(clean_series.str.match(REGEX_NUMBER))
+        if match_number_result:
+            if clean_series.str.contains(REGEX_FLOAT_NEW_SYM).sum().compute():
                 mdtype = ColumnType.NUMBER_WITH_DECIMALS
             else:
                 lowercase_column_name = column_name.lower()
-                correct_phone_nums = clean_series.str.match(REGEX_PHONE_NUMBER).sum()
-                correct_zip_codes = clean_series.str.match(REGEX_ZIP_CODE).sum()
+                correct_phone_nums = clean_series.str.match(REGEX_PHONE_NUMBER).sum().compute()
+                correct_zip_codes = clean_series.str.match(REGEX_ZIP_CODE).sum().compute()
                 if correct_phone_nums / length >= NUMBER_TYPE_MATCHES_THRESHOLD and str_in_set(
                     lowercase_column_name, RESERVED_PHONE_NUMBER_WORDS
                 ):
@@ -169,42 +193,47 @@ def infer_object_type(series, column_name, kwargs):
                 else:
                     mdtype = ColumnType.NUMBER
         else:
-            matches = clean_series.str.match(REGEX_DATETIME).sum()
+            with timer('column_type_detector.infer_object_type.part3', dict(column_name=column_name), verbose=VERBOSE):
+                matches = clean_series.str.match(REGEX_DATETIME).sum().compute()
             if matches / length >= DATETIME_MATCHES_THRESHOLD:
                 mdtype = ColumnType.DATETIME
             else:
-                correct_emails = clean_series.str.match(REGEX_EMAIL).sum()
-                correct_phone_nums = clean_series.str.match(REGEX_PHONE_NUMBER).sum()
-                correct_zip_codes = clean_series.str.match(REGEX_ZIP_CODE).sum()
-                lowercase_column_name = column_name.lower()
-                if correct_emails / length >= STRING_TYPE_MATCHES_THRESHOLD:
-                    mdtype = ColumnType.EMAIL
-                elif correct_phone_nums / length >= STRING_TYPE_MATCHES_THRESHOLD and str_in_set(
-                    lowercase_column_name, RESERVED_PHONE_NUMBER_WORDS
-                ):
-                    mdtype = ColumnType.PHONE_NUMBER
-                elif correct_zip_codes / length >= STRING_TYPE_MATCHES_THRESHOLD and str_in_set(
-                    lowercase_column_name, RESERVED_ZIP_CODE_WORDS
-                ):
-                    mdtype = ColumnType.ZIP_CODE
-                elif series_nunique == 2:
-                    mdtype = ColumnType.TRUE_OR_FALSE
-                else:
-                    if type(exact_dtype) is list:
-                        mdtype = ColumnType.TEXT
-                    elif clean_series_nunique / length >= 0.8:
-                        mdtype = ColumnType.TEXT
+                with timer('column_type_detector.infer_object_type.part4_1', dict(column_name=column_name), verbose=VERBOSE):
+                    clean_series_email = clean_series[clean_series.str.contains('@', regex=False)]
+                    correct_emails = clean_series_email.str.match(REGEX_EMAIL).sum().compute()
+                with timer('column_type_detector.infer_object_type.part4_2', dict(column_name=column_name), verbose=VERBOSE):
+                    correct_phone_nums = clean_series.str.match(REGEX_PHONE_NUMBER).sum().compute()
+                with timer('column_type_detector.infer_object_type.part4_3', dict(column_name=column_name), verbose=VERBOSE):
+                    correct_zip_codes = clean_series.str.match(REGEX_ZIP_CODE).sum().compute()
+                    lowercase_column_name = column_name.lower()
+                    if correct_emails / length >= STRING_TYPE_MATCHES_THRESHOLD:
+                        mdtype = ColumnType.EMAIL
+                    elif correct_phone_nums / length >= STRING_TYPE_MATCHES_THRESHOLD and str_in_set(
+                        lowercase_column_name, RESERVED_PHONE_NUMBER_WORDS
+                    ):
+                        mdtype = ColumnType.PHONE_NUMBER
+                    elif correct_zip_codes / length >= STRING_TYPE_MATCHES_THRESHOLD and str_in_set(
+                        lowercase_column_name, RESERVED_ZIP_CODE_WORDS
+                    ):
+                        mdtype = ColumnType.ZIP_CODE
+                    elif series_nunique == 2:
+                        mdtype = ColumnType.TRUE_OR_FALSE
                     else:
-                        word_count = clean_series.map(lambda x: len(str(x).split(' '))).max()
-                        if word_count > MAXIMUM_WORD_LENGTH_FOR_CATEGORY_FEATURES:
+                        if type(exact_dtype) is list:
+                            mdtype = ColumnType.TEXT
+                        elif clean_series_nunique / length >= 0.8:
                             mdtype = ColumnType.TEXT
                         else:
-                            if clean_series_nunique <= kwargs.get(
-                                'category_cardinality_threshold', 255
-                            ):
-                                mdtype = ColumnType.CATEGORY
+                            word_count = clean_series.map(lambda x: len(str(x).split(' '))).max().compute()
+                            if word_count > MAXIMUM_WORD_LENGTH_FOR_CATEGORY_FEATURES:
+                                mdtype = ColumnType.TEXT
                             else:
-                                mdtype = ColumnType.CATEGORY_HIGH_CARDINALITY
+                                if clean_series_nunique <= kwargs.get(
+                                    'category_cardinality_threshold', 255
+                                ):
+                                    mdtype = ColumnType.CATEGORY
+                                else:
+                                    mdtype = ColumnType.CATEGORY_HIGH_CARDINALITY
     return mdtype
 
 
