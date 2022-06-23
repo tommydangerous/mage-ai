3a9e0b86c3481d8ef3f290ee97608c2cf4988891
Johnson Kwok <johnson@mage.ai>
2022-06-22 21:50:26 -0700

    Revert "[jk] Manually update basepath config"
    This reverts commit ced308a06e474c54c8178c53721d4e47bc0ff2b6.

--- mage_ai/frontend/next.config.js
+++ mage_ai/frontend/next.config.js
@@ -1,6 +1,18 @@
 const removeImports = require('next-remove-imports')();
 
 module.exports = removeImports({
+  async rewrites() {
+    return [
+      {
+        source: '/proxy/5789/datasets',
+        destination: '/datasets',
+      },
+      {
+        source: '/proxy/5789',
+        destination: '/datasets',
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
